1c409c3b20f9db1120276c9a3d05f461666efae0
AlessioAbbati <alex_117@live.it>
2023-06-07 15:34:31 +0200

    add bootstrap

--- index.php
+++ index.php
@@ -48,6 +48,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- link di BOOTSTRAP, per accedere alla libreria-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- script con link di BOOTSTRAP, da mettere in fondo al body o nell'head con defer -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N"
+    crossorigin="anonymous" defer></script>
     <title>PHP Hotel</title>
 </head>
 <body>
